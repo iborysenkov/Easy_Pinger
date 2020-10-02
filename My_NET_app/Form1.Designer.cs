86467b46366e8fd5e5083c75bb4c3260b6f5c2f1
Evgeniy Borysenkov <iborysenkov@outlook.com>
2020-10-02 18:22:14 +0300

    Add project files.

---
+++ My_NET_app/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace My_NET_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Location = new System.Drawing.Point(39, 150);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(589, 234);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ping";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(39, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "192.168.0.240";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(292, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.ShortcutsEnabled = false;
+            this.textBox3.Size = new System.Drawing.Size(336, 132);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP config";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "IP adress for Ping";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(553, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 454);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "NetChecker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ My_NET_app/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_NET_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetMacAddress(string ipAddress)
+        {
+            string macAddress = string.Empty;
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = "arp";
+            pProcess.StartInfo.Arguments = "-a " + ipAddress;
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.CreateNoWindow = true;
+            pProcess.Start();
+            string strOutput = pProcess.StandardOutput.ReadToEnd();
+            string[] substrings = strOutput.Split('-');
+            if (substrings.Length >= 8)
+            {
+                macAddress = substrings[3].Substring(Math.Max(0, substrings[3].Length - 2))
+                         + "-" + substrings[4] + "-" + substrings[5] + "-" + substrings[6]
+                         + "-" + substrings[7] + "-"
+                         + substrings[8].Substring(0, 2);
+                return macAddress;
+            }
+
+            else
+            {
+                return "not found";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+ {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress ip_Checked;
+            string IP_address_CHK;
+            IP_address_CHK = textBox2.Text;
+            string hostName = Dns.GetHostName(); // Retrive the Name of HOST  
+                                                 //Console.WriteLine(hostName);
+                                                 // Get the IP 
+           
+            
+            
+            bool ValidateIP = IPAddress.TryParse(IP_address_CHK, out ip_Checked);
+            if (ValidateIP)
+            {
+                try
+                {
+                    Ping myPing = new Ping();
+                    PingReply reply = myPing.Send(ip_Checked, 1000);
+                    if (reply != null)
+                    {
+                        textBox1.Text += "Ping " + ip_Checked.ToString() + System.Environment.NewLine;
+                        textBox1.Text += DateTime.Now.ToString() + " " + reply.Status + " Time : " + reply.RoundtripTime.ToString() + " Address : " + reply.Address + Environment.NewLine;
+                        //Console.WriteLine(reply.ToString());
+
+                        string Test_on_Success = reply.Status.ToString();
+
+                        bool result = Test_on_Success.Equals("Success");
+                        if (result)
+                        {
+                            string ipAddress = ip_Checked.ToString();
+                            string macAddress = string.Empty;
+                            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+                            pProcess.StartInfo.FileName = "arp";
+                            pProcess.StartInfo.Arguments = "-a " + ipAddress;
+                            pProcess.StartInfo.UseShellExecute = false;
+                            pProcess.StartInfo.RedirectStandardOutput = true;
+                            pProcess.StartInfo.CreateNoWindow = true;
+                            pProcess.Start();
+                            string strOutput = pProcess.StandardOutput.ReadToEnd();
+                            string[] substrings = strOutput.Split('-');
+                            if (substrings.Length >= 8)
+                            {
+                                macAddress = substrings[3].Substring(Math.Max(0, substrings[3].Length - 2))
+                                         + "-" + substrings[4] + "-" + substrings[5] + "-" + substrings[6]
+                                         + "-" + substrings[7] + "-"
+                                         + substrings[8].Substring(0, 2);
+                                textBox1.Text += "MAC address: " + macAddress + Environment.NewLine;
+                            }
+
+                            else
+                            {
+                                textBox1.Text += "not found"+ Environment.NewLine;
+                            }
+                        }
+
+                    }
+
+                    
+
+                    textBox3.Text = String.Empty;
+
+                    IPHostEntry host = Dns.GetHostEntry(hostName);
+
+                    textBox3.Text += $"GetHostEntry({hostName}) returns:";
+
+                    foreach (IPAddress address in host.AddressList)
+                    {
+                        textBox3.Text += $"    {address}" + System.Environment.NewLine;
+                    }
+
+
+
+
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR: You have Some TIMEOUT issue");
+                }
+
+            }
+            else
+                MessageBox.Show("This is not a valide ip address, enter in format XXX.XXX.XXX.XXX");
+
+            //Console.WriteLine("My IP Address is :" + myIP);
+
+            
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
