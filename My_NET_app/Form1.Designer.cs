930720dc5e795509dff18542afec1c8fa18ec829
Evgeniy Borysenkov <iborysenkov@outlook.com>
2020-10-21 16:47:08 +0300

    Added bigger buttoms.

--- My_NET_app/Form1.Designer.cs
+++ My_NET_app/Form1.Designer.cs
@@ -48,16 +48,16 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(599, 633);
+            this.textBox1.Size = new System.Drawing.Size(599, 329);
             this.textBox1.TabIndex = 1;
             this.textBox1.WordWrap = false;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(292, 905);
+            this.button1.Location = new System.Drawing.Point(505, 605);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(123, 37);
             this.button1.TabIndex = 2;
             this.button1.Text = "Ping";
             this.button1.UseVisualStyleBackColor = true;
@@ -105,9 +105,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1157, 905);
+            this.button2.Location = new System.Drawing.Point(1109, 605);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(123, 37);
             this.button2.TabIndex = 9;
             this.button2.Text = "Clear";
             this.button2.UseVisualStyleBackColor = true;
@@ -120,7 +120,7 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
             this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox4.Size = new System.Drawing.Size(594, 858);
+            this.textBox4.Size = new System.Drawing.Size(594, 554);
             this.textBox4.TabIndex = 10;
             // 
             // label2
@@ -148,7 +148,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1244, 940);
+            this.ClientSize = new System.Drawing.Size(1244, 654);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox4);
